d25024354e9e31c66a1b05ad7668f602c83c9ff8
Tyler Smart <tyler@smarts.io>
2016-05-05 18:05:26 -0700

    Tyrant should use Sha512 for our purposes. TODO make this extensible later, i.e break it out into a separate, pluggable class and DI, brah

--- lib/tyrant/authenticatable.rb
+++ lib/tyrant/authenticatable.rb
@@ -6,25 +6,24 @@ module Tyrant
     feature Default
     feature Sync # FIXME: really?
 
-    property :auth_meta_data, default: Hash.new do
-      include Struct
-      property :confirmation_token
-      property :confirmed_at
-      property :confirmation_created_at
-      property :password_digest
-    end
+    property :confirmation_token
+    property :confirmed_at
+    property :confirmation_sent_at
+    property :encrypted_password
+    property :password_salt
+    property :remember_token
 
     module Confirm
       def confirmable!
-        auth_meta_data.confirmation_token = SecureRandom.urlsafe_base64
-        auth_meta_data.confirmation_created_at = DateTime.now
+        confirmation_token = SecureRandom.urlsafe_base64
+        confirmation_created_at = DateTime.now
         self
       end
 
       # without token, this decides whether the user model can be activated (e.g. via "set a password").
       # with token, this additionally tests if the token is correct.
       def confirmable?(token=false)
-        persisted_token = auth_meta_data.confirmation_token
+        persisted_token = confirmation_token
 
         # TODO: add expiry etc.
         return false unless (persisted_token.is_a?(String) and persisted_token.size > 0)
@@ -35,41 +34,68 @@ module Tyrant
 
       # alias_method :confirmed?, :confirmable?
       def confirmed?
-        not auth_meta_data.confirmed_at.nil?
+        not confirmed_at.nil?
       end
 
       def confirmed!(confirmed_at=DateTime.now)
-        auth_meta_data.confirmation_token = nil
-        auth_meta_data.confirmed_at       = confirmed_at # TODO: test optional arg.
+        confirmation_token = nil
+        confirmed_at       = confirmed_at # TODO: test optional arg.
       end
 
       def confirmation_token
-        auth_meta_data.confirmation_token
+        confirmation_token
       end
 
     private
       def compare_token(token)
-        token == auth_meta_data.confirmation_token
+        token == confirmation_token
       end
     end # Confirm
     include Confirm
 
 
-    require "bcrypt"
+    # we are using SHA512 so use this instead of Bcrypt
+
+    require "digest/sha2"
     module Digest
       def digest
-        return unless auth_meta_data.password_digest
-        BCrypt::Password.new(auth_meta_data.password_digest)
+        return unless encrypted_password && password_salt
+        digest = [password, salt].flatten.join('')
+        stretches.times { digest = ::Digest::SHA512.hexdigest(digest) }
+        digest
       end
 
       def digest!(password)
-        auth_meta_data.password_digest = BCrypt::Password.create(password)
+        # TODO convert to SHA512 when we are doing creation (right now only login)
+        password_digest = BCrypt::Password.create(password)
       end
 
       def digest?(password)
-        digest == password
+        encrypted_password == sha_512_digest(password, 20, password_salt, nil)
+      end
+
+      def sha_512_digest(password, stretches = 20, salt, pepper)
+        digest = [password, salt].flatten.join('')
+        stretches.times { digest = ::Digest::SHA512.hexdigest(digest) }
+        digest
       end
     end
+
+    # require "bcrypt"
+    # module Digest
+    #   def digest
+    #     return unless password_digest
+    #     BCrypt::Password.new(password_digest)
+    #   end
+    #
+    #   def digest!(password)
+    #     password_digest = BCrypt::Password.create(password)
+    #   end
+    #
+    #   def digest?(password)
+    #     digest == password
+    #   end
+    # end
     include Digest
 
   end
